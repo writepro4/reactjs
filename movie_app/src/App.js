86f50d5be369d126d751f9a51f080d99c6791d6a
writepro4 <writepro4@naver.com>
2020-06-05 16:49:54 +0900

    [update] App.js componentDidMount()사용

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,37 +1,18 @@
 import React from "react";
 
 class App extends React.Component {
-    //state를 사용할려고 클래스 App을 사용하는 것도 있다.
-    //변할 수 있는 데이터는 state를 사용한다.
     state = {
-        count: 0
-    };
-    add = () => {
-        this.setState(current => ({ count: current.count + 1 }));
-    };
-    minus = () => {
-        this.setState(current => ({ count: current.count - 1 }));
+        isLoading: true,
+        movies: []
     };
     componentDidMount() {
-        console.log("Component rendered");
+        setTimeout(() => {
+            this.setState({ isLoading: false });
+        }, 6000);
     }
-    componentDidUpdate() {
-        console.log("I just updated");
-    }
-    componentWillUnmount() {
-        console.log("Goodbye, cruel world");
-    }
-
     render() {
-        console.log("I'm rendering");
-        //함수()는 즉시 나타냄, 함수 는 눌러야만 작동함.
-        return (
-            <div>
-                <h1>The number is: {this.state.count}</h1>
-                <button onClick={this.add}>Add</button>
-                <button onClick={this.minus}>Minus</button>
-            </div>
-        );
+        const { isLoading } = this.state;
+        return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
     }
 }
 
