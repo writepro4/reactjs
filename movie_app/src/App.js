8090044cda692ad3e2172528f0e41171862b38d9
writepro4 <writepro4@naver.com>
2020-06-05 14:18:37 +0900

    [update] App.js props개념 컴포넌트 사용

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import Potato from "./Potato";
+
+//리액트에서는 {}인자값을 받을때 대괄호를 추가
+//props에서 가져올때는 props.favourite 할 필요없이
+//{favourite} es6문법을 이용해서 가져오면 됨.
+function Food({ favourite }) {
+    return <h1>I like {favourite}</h1>;
+}
 
 function App() {
-  return (
-      <div>
-        <h1>Hello</h1>
-          <Potato />
+    return (
+        <div>
+            <h1>Hello</h1>
+            <Food favourite="kimchi" />
+            <Food favourite="ramen" />
+            <Food favourite="samgiopsal" />
+            <Food favourite="chukumi" />
       </div>
   );
 }
