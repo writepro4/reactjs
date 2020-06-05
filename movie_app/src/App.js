09853e7055228d3ebda181ad4a611c932959047d
writepro4 <writepro4@naver.com>
2020-06-05 18:21:29 +0900

    [add] App.css css 스타일 추가

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
     state = {
@@ -10,33 +11,41 @@ class App extends React.Component {
     getMovies = async () => {
         const {
             data: {
-                data: { movies }
+                data: {movies}
             }
         } = await axios.get(
             "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
         );
-        this.setState({ movies, isLoading: false });
+        this.setState({movies, isLoading: false});
     };
+
     componentDidMount() {
         this.getMovies();
     }
+
     render() {
-        const { isLoading, movies } = this.state;
+        const {isLoading, movies} = this.state;
         return (
-            <div>
-                {isLoading
-                    ? "Loading..."
-                    : movies.map(movie => (
-                        <Movie
-                            key={movie.id}
-                            id={movie.id}
-                            year={movie.year}
-                            title={movie.title}
-                            summary={movie.summary}
-                            poster={movie.medium_cover_image}
-                        />
-                    ))}
-            </div>
+            <section class="container">
+                {isLoading ? (
+                    <div class="loader">
+                        <span class="loader__text">Loading...</span>
+                    </div>
+                ) : (
+                    <div class="movies">
+                        {movies.map(movie => (
+                            <Movie
+                                key={movie.id}
+                                id={movie.id}
+                                year={movie.year}
+                                title={movie.title}
+                                summary={movie.summary}
+                                poster={movie.medium_cover_image}
+                            />
+                        ))}
+                    </div>
+                )}
+            </section>
         );
     }
 }
