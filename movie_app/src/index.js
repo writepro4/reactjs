4f7b2dd7b56430453bf5ff647f947f80380feb9a
writepro4 <writepro4@naver.com>
2020-06-05 14:07:29 +0900

    [add] Potato.js 리액트 컴포넌트 파일 생성

--- movie_app/src/index.js
+++ movie_app/src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//하나의 컴포넌트만 렌더링 하기 때문에 컴포넌트를 추가 하고 싶다면
+//렌더링한 컴포넌트에 추가시켜야 함.
+ReactDOM.render(<App />, document.getElementById("potato"));
