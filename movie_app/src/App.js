2ae5786cbbc218bc20b7123ff93ce4c57c6312ce
writepro4 <writepro4@naver.com>
2020-06-05 15:39:24 +0900

    [update] App.js 함수에서 클래스로 변경/state 사용

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,69 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const foodILike = [
-    {
-        id: 1,
-        name: "Kimchi",
-        image:
-            "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-        rating: 4.9
-    },
-    {
-        id: 3,
-        name: "Bibimbap",
-        image:
-        "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-        rating: 4.8
-    },
-    {
-        id: 4,
-        name: "Doncasu",
-        image:
-        "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-        rating: 5.5
-    },
-    {
-        id: 5,
-        name: "Kimbap",
-        image:
-        "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-        rating: 4.7
+class App extends React.Component {
+    //state를 사용할려고 클래스 App을 사용하는 것도 있다.
+    //변할 수 있는 데이터는 state를 사용한다.
+    state = {
+        count: 0
+    };
+    add = () => {
+        console.log("add");
+    };
+    minus = () => {
+        console.log("minus");
+    };
+
+    render() {
+        //함수()는 즉시 나타냄, 함수 는 눌러야만 작동함.
+        return (
+            <div>
+                <h1>The number is: {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
     }
-];
-
-function Food({ name, picture, rating }) {
-    return (
-        <div>
-            <h2>I like {name}</h2>
-            <h4>{rating}/5.0</h4>
-            <img src={picture} alt={name} />
-        </div>
-    );
-}
-
-//변수타입 체크용 String이면 String만 값이 올 수 있게 설정?
-//propTypes 이름을 변경할 수 는 없다.
-Food.propTypes = {
-    name: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
-    rating: PropTypes.number
-};
-
-
-function App() {
-    return (
-        <div>
-            {foodILike.map(dish => (
-                <Food
-                    key={dish.id}
-                    name={dish.name}
-                    picture={dish.image}
-                    rating={dish.rating}
-                />
-            ))}
-      </div>
-  );
 }
 
 export default App;
